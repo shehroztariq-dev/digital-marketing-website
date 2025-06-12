0e1b8bfb30d89ad6feb19add4c494ba756a962c3
Shehroz Tariq <shehroztariq.dev@gmail.com>
2025-06-12 12:57:28 +0500

    evault card update

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["flowbite.s3.amazonaws.com"],
+    domains: ["flowbite.s3.amazonaws.com", "images.unsplash.com"],
   },
 };
 
